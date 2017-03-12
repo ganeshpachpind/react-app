a85fe954652aa36c6b6d6b8fa193724e19fd3dd1
ganeshp <pachpindg@gmail.com>
2017-03-12 22:04:59 +0530

    Example of props and state

--- app/js/HelloComponet.js
+++ app/js/HelloComponet.js
@@ -1,8 +1,41 @@
 import React from 'react'
 class Hello extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            txt: "state value by using constructor"
+        }
+    }
+
+    update(e) {
+        this.setState({
+            txt: e.target.value
+        });
+    }
+
     render() {
-        return (<h1>This is class Component which can have State </h1>);
+        return (
+            <div>
+                <input type="text" onChange={this.update.bind(this)}/><br/>
+                <b>Prop values are managed by the parent component , State values are managed by current component</b>
+                <b>Value of prop someNum </b> {this.props.someNum}<br/>
+                <b>Value of prop someTxt </b> {this.props.someTxt}<br/>
+                <b>Value of state TXT </b> {this.state.txt}<br/>
+                <h1>This is class Component which can have State </h1>
+            </div>
+        );
     }
 }
+
+Hello.propTypes = {
+    someNum: React.PropTypes.number.isRequired,
+    someTxt: React.PropTypes.string
+};
+
+
+Hello.defaultProps = {
+    someTxt: "This is default text"
+};
+
 
 export default Hello
--- app/js/app.js
+++ app/js/app.js
@@ -1,4 +1,4 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Hello from './HelloComponet'
-ReactDOM.render(<Hello/>, document.getElementById("app"));
+ReactDOM.render(<Hello someNum={10}/>, document.getElementById("app"));
